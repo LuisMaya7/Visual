47988bbf581692a16d55aeb739489aea68ebf5c5
Luis Maya <luisfelipemayaq@gmail.com>
2024-01-06 19:24:28 -0500

    Grafica 3

--- Grafica_3.py
+++ Grafica_3.py
@@ -1,6 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
 
 data = {
     'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
@@ -15,18 +14,17 @@ data = {
 
 df = pd.DataFrame(data)
 
-plt.figure(figsize=(10, 6))
-plt.boxplot([df[df['materia'] == materia]['nota'] for materia in df['materia'].unique()],
-            labels=df['materia'].unique())
-plt.title('Distribución de Notas por Materia')
-plt.xlabel('Materias')
-plt.ylabel('Notas')
-plt.grid(True)
-plt.show()
+fig_boxplot = px.box(df, x='materia', y='nota', color='materia', 
+                     title='Grafico de cajas',
+                     labels={'nota': 'Valores'},
+                     category_orders={'materia': ['Matemáticas', 'Historia', 'Ciencias', 'Lenguaje']},
+                     color_discrete_sequence=px.colors.qualitative.Set2)
 
-colors = ['c', 'red']
-startangle= 45
-plt.pie(df['aprobado'].value_counts(), labels=['Aprobados', 'No Aprobados'], autopct='%1.1f%%', colors=colors, startangle=startangle)
-plt.title('Distribución de Aprobados y No Aprobados')
-plt.show()
 
+fig_boxplot.update_layout(xaxis_title='Materia', yaxis_title='Nota')
+
+fig_boxplot.show()
+
+fig_pie_chart = px.pie(df, df['aprobado'], title='Grafico de torta')
+
+fig_pie_chart.show()
